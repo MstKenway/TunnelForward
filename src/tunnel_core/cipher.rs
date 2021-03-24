25e44b65e5926632dafc26c6305db5455007bd8c
MstKenway <mstkenway@gmail.com>
2021-03-24 14:31:53 +0800

    First Commit

--- src/tunnel_core/cipher.rs
+++ src/tunnel_core/cipher.rs
@@ -137,3 +137,39 @@ pub fn decrypt_aesgcm(input: &[u8], output: &mut [u8], key: &[u8], iv: &[u8]) ->
     }
     Ok(data_len)
 }
+
+#[cfg(test)]
+fn gcm_test() {
+    use rand::RngCore;
+    use rand::rngs::OsRng;
+    let message = "This is another test.";
+    println!("{}", message);
+
+    let mut key: [u8; 16] = [0; 16];
+    let mut iv: [u8; 12] = [0; 12];
+
+    // In a real program, the key and iv may be determined
+    // using some other mechanism. If a password is to be used
+    // as a key, an algorithm like PBKDF2, Bcrypt, or Scrypt (all
+    // supported by Rust-Crypto!) would be a good choice to derive
+    // a password. For the purposes of this example, the key and
+    // iv are just random values.
+    // let mut rng = OsRng::new().ok().unwrap();
+    OsRng.fill_bytes(&mut key);
+    OsRng.fill_bytes(&mut iv);
+
+
+    let mut encrypted_data = vec![0; message.len() + 16];
+    let mut result = encrypt_aesgcm(message.as_bytes(), &mut encrypted_data, &key, &iv).ok().unwrap_or(0);
+    println!("cipher text: {:?}", encrypted_data);
+
+    // encrypted_data[0] = 0u8;
+
+    let mut decrypted_data = vec![0; message.len()];
+    result = decrypt_aesgcm(&encrypted_data[..], &mut decrypted_data, &key, &iv).ok().unwrap_or(0);
+    let s = match String::from_utf8(decrypted_data) {
+        Ok(v) => v,
+        Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
+    };
+    println!("decrpyted text: {:}", s);
+}
