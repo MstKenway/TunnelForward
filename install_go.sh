a9fa6769a6c74f8e8ed433c8bbee4fb42588063e
Yun Gao <814639168@qq.com>
2021-04-05 22:34:13 +0800

    Create install_go.sh

---
+++ install_go.sh
@@ -0,0 +1,22 @@
+wget -o /usr/bin/TunnelForward https://github.com/MstKenway/TunnelForward/releases/download/0.0.5/TunnelForward_Go_linux_amd64
+chmod +x /usr/bin/TunnelForward
+
+
+cat >/etc/systemd/system/TunnelForward.service << EOF
+[Unit]
+Description=TunnelForward - Forwarding traffic
+After=network.target nss-lookup.target network-online.target
+Wants=network-online.target
+  
+[Service]
+Type=simple
+PIDFile=/run/TunnelForward.pid
+ExecStart=/usr/bin/TunnelForward -f /etc/TunnelForward/config.json >/dev/null &2>1
+PrivateTmp=true
+  
+[Install]
+WantedBy=multi-user.target
+EOF
+
+systemctl daemon-reload
+systemctl enable TunnelForward
