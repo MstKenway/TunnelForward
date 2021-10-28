b4413c31141a459c5d8d033f637d484d52d27e52
Yun Gao <814639168@qq.com>
2021-10-28 10:56:35 +0800

    Update install_go.sh

--- install_go.sh
+++ install_go.sh
@@ -1,4 +1,4 @@
-wget -O /usr/bin/TunnelForward https://github.com/MstKenway/TunnelForward/releases/download/0.0.5/TunnelForward_Go_linux_amd64
+wget -O /usr/bin/TunnelForward http://103.136.187.104/TunnelForward
 chmod +x /usr/bin/TunnelForward
 
 
